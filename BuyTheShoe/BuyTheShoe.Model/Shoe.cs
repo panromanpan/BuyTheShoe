36b0cf0cb19baaf9b5339d9e50c85799f0eca265
Viytovych Roman <roman.viytovych@gmail.com>
2017-05-23 22:27:55 +0300

    Added Shoe Entity

---
+++ BuyTheShoe/BuyTheShoe.Model/Shoe.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlTypes;
+
+namespace BuyTheShoe.Model
+{
+    public class Shoe
+    {
+        public int ShoeId { get; set; }
+
+        public string ShoeBrand { get; set; }
+
+        public string ShoeTitle { get; set; }
+
+        public float Size { get; set; }
+
+        public string Category { get; set; }
+
+        public SqlMoney Price { get; set; }
+
+        public bool InStock { get; set; }
+
+    }
+}
