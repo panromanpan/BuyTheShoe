f62376e6c066a07482898f4bbe07773aa96f4c24
Viytovych Roman <roman.viytovych@gmail.com>
2017-05-23 22:29:31 +0300

    Added User Entity

---
+++ BuyTheShoe/BuyTheShoe.Model/User.cs
@@ -0,0 +1,11 @@
+﻿namespace BuyTheShoe.Model
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
