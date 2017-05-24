dc71373f7e01b27cc891df598c80ce26e12472d5
Viytovych Roman <roman.viytovych@gmail.com>
2017-05-24 10:44:44 +0300

    Added ShoesRepository

---
+++ BuyTheShoe/BuyTheShoe.DAL/IShoesRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BuyTheShoe.DAL
+{
+    public interface IShoesRepository
+    {
+        void DeleteShoe(Shoe shoe);
+        Shoe GetAShoe();
+        Shoe GetShoeById(int id);
+        List<Shoe> GetShoes();
+        void UpdateShoe(Shoe shoe);
+    }
+}
---
+++ BuyTheShoe/BuyTheShoe.DAL/ShoesRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyTheShoe.DAL
+{
+    class ShoesRepository : IShoesRepository
+    {
+        public void DeleteShoe(Shoe shoe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Shoe GetAShoe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Shoe GetShoeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Shoe> GetShoes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateShoe(Shoe shoe)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
