ab0ad03f0a42470931f24c14e4409251673593a8
Viytovych Roman <roman.viytovych@gmail.com>
2017-05-24 11:07:56 +0300

    Added ShoesDataServices

---
+++ BuyTheShoe/BuyTheShoe/Services/IShoesDataService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using BuyTheShoe.Model;
+namespace BuyTheShoe.Services
+{
+    public interface IShoesDataService
+    {
+        void DeleteShoe(Shoe shoe);
+        List<Shoe> GetAllShoes();
+        Shoe GetShoeDetail(int shoeId);
+        void UpdateShoe(Shoe shoe);
+    }
+}
---
+++ BuyTheShoe/BuyTheShoe/Services/ShoesDataService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using BuyTheShoe.DAL;
+namespace BuyTheShoe.Services
+{
+    public class ShoesDataService : IShoesDataService
+    {
+        private readonly IShoesRepository _repository;
+
+        public ShoesDataService(IShoesRepository repository)
+        {
+            this._repository = repository;
+        }
+        public void DeleteShoe(Shoe shoe)
+        {
+            _repository.DeleteShoe(shoe);
+        }
+
+        public List<Shoe> GetAllShoes()
+        {
+            return _repository.GetShoes();
+        }
+
+        public Shoe GetShoeDetail(int shoeId)
+        {
+            return _repository.GetShoeById(shoeId);
+        }
+
+        public void UpdateShoe(Shoe shoe)
+        {
+            _repository.UpdateShoe(shoe);
+        }
+    }
+}
